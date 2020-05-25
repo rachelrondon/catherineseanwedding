1c6dc6d4631b1f7e762604156d3fc53b152b8e34
Rachel Rondon <rachel.sun.rondon@gmail.com>
2020-05-24 22:26:00 -0400

    add travel and schedule

--- catherinesean/src/App.js
+++ catherinesean/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './landingpage';
+import Schedule from './schedule';
 import NavBar from './navbar.js';
-import rosecliff from '../public/rosecliff_mansion.jpg';
+import Travel from './travel.js';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <BrowserRouter>
         <NavBar />
+        <Switch>
+          <Route path="/" component={LandingPage} exact />
+          <Route path="/schedule" component={Schedule} exact />
+          <Route path="/travel" component={Travel} exact /> 
+        </Switch>
+        </BrowserRouter>
       </div>
     );
   }
---
+++ catherinesean/src/navbar.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Typography from '@material-ui/core/Typography';
+
+const NavBar = () => {
+
+  return (
+    <div className="nav-bar">
+      <AppBar>
+        <Toolbar>
+          <IconButton className="">
+
+          </IconButton>
+          <div className="date-details">
+          <Typography className="wedding-date">
+            May 29, 2021
+          </Typography>
+          <Typography className="wedding-location">
+            Newport, Rhode Island
+          </Typography>
+          </div>
+          <Typography className="title">
+            Catharine & Sean
+          </Typography>
+          <div className="links">
+            <Typography className="link">
+              <a href="/">Home</a>
+            </Typography>
+            <Typography className="link">
+              <a href="/schedule">Schedule</a>
+            </Typography>
+            <Typography className="link">
+              <a href="/travel">Travel</a>
+            </Typography>
+            <Typography className="link">
+              <a href="">Registry</a>
+            </Typography>
+            <Typography className="link">
+              <a href="">Wedding Party</a>
+            </Typography>
+            <Typography className="link">
+              <a href="">Photos</a>
+            </Typography>
+            <Typography className="link">
+              <a href="">Things To Do</a>
+            </Typography>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
+
+export default NavBar;
